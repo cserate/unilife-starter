8c919d635a6554e208947485cb3838b05b842d1e
Candice Serate <cwserate@gmail.com>
2022-12-12 23:02:11 -0500

    trying to add static image unsuccessfully

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Homepage from './pages/Homepage/Homepage';
+
 
 
 function App() {
@@ -14,7 +17,12 @@ function App() {
 
   return (
     <div className='App'>
+      <BrowserRouter>
       <Header /> 
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Homepage/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Banner from '../../components/Banner/Banner'
+import './Homepage.css'
+
+function Homepage() {
+  return (
+    <div>Homepage</div>
+  )
+}
+
+export default Homepage
